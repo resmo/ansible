48cc48376847c55e2e4ab53b632c4fe3621622d9
Rene Moser <mail@renemoser.net>
2017-02-27 21:12:32 +0100

    reorganize to targets

--- test/integration/targets/cloudstack/runme.sh
+++ test/integration/targets/cloudstack/runme.sh
@@ -6,7 +6,5 @@ set -eu
 
 docker pull resmo/cloudstack-sim
 docker run --name cloudstack -d -p 8888:8888 resmo/cloudstack-sim
-
-make cloudstack TEST_FLAGS="--tags test_cs_user"
-
+ansible-playbook cloudstack.yml -v "$@"
 docker rm -f cloudstack
